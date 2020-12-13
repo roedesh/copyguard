31d86a51455d30dfd453888c7b2be7e6b55bee31
Ruud Schroën <mail@ruud.je>
2020-12-13 15:46:04 +0100

    Add test for hidden text contents

--- specs/contentScript/handleCopyEvent.spec.ts
+++ specs/contentScript/handleCopyEvent.spec.ts
@@ -1,28 +1,40 @@
 import handleCopyEvent from "../../src/contentScript/handleCopyEvent";
 
-const setupBody = () => {
-  document.body.innerHTML = `<div><p>Text to select</p></div>`;
+const setupBody = (extraHTML = "") => {
+  document.body.innerHTML = `<div><p>Text to select${extraHTML}</p></div>`;
 };
 
-const selectText = (window: Window, element: Node): void => {
+const selectElement = (querySelector: string): void => {
   const range = window.document.createRange();
+  const element = document.querySelector(querySelector);
   range.selectNode(element);
+  window.getSelection().empty();
   window.getSelection().addRange(range);
 };
 
 describe("handleCopyEvent", () => {
-  it("given a text selection, calls sendMessage when the copy event is fired", async () => {
+  it("given a text selection, calls sendMessage", async () => {
     setupBody();
-
-    const paragraph = window.document.querySelector("p");
-    selectText(window, paragraph);
+    selectElement("p");
 
     mockBrowser.runtime.sendMessage.expect({ selection: "Text to select", hasHiddenElementsInSelection: false });
 
     handleCopyEvent();
   });
 
-  it("given no text selection, does not call sendMessage when the copy event is fired", async () => {
+  it("given a text selection with hidden contents, calls sendMessage with hasHiddenElementsInSelection set to true", async () => {
+    setupBody(`<span style="font-size: 0px;">hidden text</span>`);
+    selectElement("p");
+
+    mockBrowser.runtime.sendMessage.expect({
+      selection: "Text to selecthidden text",
+      hasHiddenElementsInSelection: true,
+    });
+
+    handleCopyEvent();
+  });
+
+  it("given no text selection, does not call sendMessage", async () => {
     setupBody();
     handleCopyEvent();
 
