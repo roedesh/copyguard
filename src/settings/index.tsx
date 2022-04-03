3048668f38a3da24d6f0a2a1192a18c8bc45bc13
Ruud Schroën <ruud.schroen@kabisa.nl>
2022-04-03 12:24:20 +0200

    Use createRoot to support React 18

--- src/settings/index.tsx
+++ src/settings/index.tsx
@@ -1,13 +1,15 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 
-ReactDOM.render(
+const rootElement = document.getElementById("app");
+const root = createRoot(rootElement);
+
+root.render(
   <HashRouter>
     <App />
   </HashRouter>,
-  document.getElementById("app"),
 );
